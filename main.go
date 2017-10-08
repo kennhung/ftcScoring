6cd9f3687731c1c23c6cbb01581ba031fa50f120
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-10-08 13:15:34 +0800

    Basic Web service

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"time"
+	"github.com/kennhung/ftcScoring/web"
+)
 
+const eventDBPATH = "./foo.db"
+const webPort = 8080;
 
 func main() {
+	log.Print("Scoring System Starting at",time.Now())
+
+	web := web.NewWeb(nil)
+	web.ServeWebInterface(webPort)
 
 }
---
+++ web/web.go
@@ -0,0 +1,45 @@
+package web
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/kennhung/ftcScoring/arena"
+	"bytes"
+	"github.com/kennhung/ftcScoring/webTemplate"
+	"time"
+	"fmt"
+)
+
+type Web struct {
+	arena *arena.Arena
+}
+
+func NewWeb(arena *arena.Arena) *Web {
+	web := &Web{arena: arena}
+	log.Print(web.arena)
+	return web
+}
+
+func (web *Web) ServeWebInterface(webPort int) {
+	http.Handle("/res/", http.StripPrefix("/res/", http.FileServer(http.Dir("res/"))))
+	http.Handle("/", web.newHandler())
+	// Start Server
+	log.Printf("Serving HTTP requests on port %d", webPort)
+	log.Print(fmt.Sprintf(":%d", webPort))
+	http.ListenAndServe(fmt.Sprintf(":%d", webPort), nil)
+}
+
+
+func (web *Web)newHandler() http.Handler {
+	router := mux.NewRouter()
+	router.HandleFunc("/", web.IndexHandler).Methods("GET")
+	return router
+}
+
+func (web *Web)IndexHandler(w http.ResponseWriter, r *http.Request){
+	buffer := new(bytes.Buffer)
+	template.Display(fmt.Sprint(time.Now()), buffer)
+
+	w.Write(buffer.Bytes())
+}
