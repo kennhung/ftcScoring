d52e2d0250f5f8b02924805b91b0b4d5991366b2
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-10-22 14:51:19 +0800

    update generate file

--- db/template/20171009120633_CreateEventSettings.sql
+++ db/template/20171009120633_CreateEventSettings.sql
@@ -4,7 +4,9 @@ CREATE TABLE event_settings (
   name                   VARCHAR(255),
   type                   VARCHAR(16),
   region                 VARCHAR(16),
-  date                   DATETIME
+  date                   DATETIME,
+  displaybackgroundcolor VARCHAR(16),
+  displayoverlaymode     BOOLEAN
 );
 
 -- +goose Down
--- db/template/20171009120657_CreateMatches.sql
+++ db/template/20171009120657_CreateMatches.sql
@@ -1,21 +1,22 @@
 -- +goose Up
 CREATE TABLE matches (
-  id              INTEGER PRIMARY KEY,
-  type            VARCHAR(16),
-  time            DATETIME,
-  red1            INT,
-  red1notshow     bool,
-  red2            INT,
-  red2notshow     bool,
-  blue1           INT,
-  blue1notshow    bool,
-  blue2           INT,
-  blue2notshow    bool,
-  status          VARCHAR(16),
-  startedat       DATETIME,
-  winner          VARCHAR(16)
+  id           INTEGER PRIMARY KEY,
+  type         VARCHAR(16),
+  displayname  VARCHAR(16),
+  time         DATETIME,
+  red1         INT,
+  red1notshow  bool,
+  red2         INT,
+  red2notshow  bool,
+  blue1        INT,
+  blue1notshow bool,
+  blue2        INT,
+  blue2notshow bool,
+  status       VARCHAR(16),
+  startedat    DATETIME,
+  winner       VARCHAR(16)
 );
-CREATE UNIQUE INDEX type_displayname ON matches(type);
+CREATE UNIQUE INDEX type_displayname ON matches(type, displayname);
 
 -- +goose Down
 DROP TABLE matches;
